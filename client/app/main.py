26bf45089b10fa64ff502bf089c7c3da3fadabb8
daisuke8000 <daisuke12108000@gmail.com>
2022-02-15 19:11:19 +0900

    main.py modify

--- client/app/main.py
+++ client/app/main.py
@@ -3,12 +3,20 @@ import pancake_pb2_grpc
 import pancake_pb2
 
 
-def run():
+def baker():
     with grpc.insecure_channel('localhost:50051') as channel:
         stub = pancake_pb2_grpc.PancakeBakerServiceStub(channel)
-        response = stub.Bake(pancake_pb2.BakeRequest(menu=1))
+        response = stub.Bake(pancake_pb2.BakeRequest(menu=2))
+        return response
+
+
+def report():
+    with grpc.insecure_channel('localhost:50051') as channel:
+        stub = pancake_pb2_grpc.PancakeBakerServiceStub(channel)
+        response = stub.Report(pancake_pb2.ReportRequest())
         return response
 
 
 if __name__ == "__main__":
-    print(run())
+    print(baker())
+    print(report())
